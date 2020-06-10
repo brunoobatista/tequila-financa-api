6a3c34928c5ea369ddc092afae7216ba2aac1ca0
Bruno Batista <brunorafael.o@hotmail.com>
2020-06-09 23:23:13 -0300

    Pre finalizado o desenvolvidomento da lista de despesas fixas

---
+++ Repositories/DespesasFixasRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Tequila.Models;
+
+namespace Tequila.Repositories
+{
+    public class DespesasFixasRepository : EFCoreRepository<DespesasFixas, ApplicationContext>
+    {
+        private readonly ApplicationContext _context;
+
+        public DespesasFixasRepository(ApplicationContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public List<DespesasFixas> getDespesasFixasByUsuario(long idUsuario)
+        {
+            return _context.DespesasFixas
+                .Where(d => d.UsuarioId == idUsuario && d.Ativo == 1)
+                .ToList();
+        }
+    }
+}
