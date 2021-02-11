c3be57eb238d2d5b6c50ec3b408f2f6a38670def
Bruno Batista <brunorafael.o@hotmail.com>
2021-02-11 02:46:04 -0300

    Adiciona Home Controller para fazer a verificação de logado

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tequila.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("home")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet()]
+        public ActionResult<string> GetCarteira([FromRoute] long usuarioId)
+        {
+            try
+            {
+                return Ok("Teste do Home");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
