a227e402f9104fd2582dbb702d3eab3a3f48a7e4
Bruno Batista <brunorafael.o@hotmail.com>
2021-08-11 20:35:48 -0300

    Ajuste trigger insert despesa variavel

--- Migration/Scripts/v004__triggers.sql
+++ Migration/Scripts/v004__triggers.sql
@@ -51,27 +51,25 @@ create trigger inserir_despesa_fixa_pela_carteira
 
 
 
-create or replace function atualizar_valor_despesa_update_despesa()
-    returns trigger as $body$
+create or replace function atualizar_valor_despesa_update_despesa() returns trigger
+    language plpgsql
+as
+$$
 declare
-    v_carteira_id bigint;
     v_new numeric;
     v_old numeric;
     v_carteira carteira;
 begin
-   if (tg_op = 'INSERT') then
-        --tipo 0 = VARIAVEL
-        if (new.tipo_id = 0) then
-            select * into v_carteira from carteira where id = new.carteira_id;
-            v_old = v_carteira.despesa - old.valor;
-            v_new = v_old + new.valor;
-            update carteira set despesa = v_new, alterado_em = now() where id = new.carteira_id;
-        end if;
+    if (tg_op = 'INSERT') then
+        select * into v_carteira from carteira where id = new.carteira_id;
+        v_new = v_carteira.despesa + new.valor;
+        update carteira set despesa = v_new where id = v_carteira.id;
+        return new;
     end if;
     if (tg_op = 'UPDATE') then
         --tipo 2 = parcelada
         --tipo 0 = VARIAVEL
-        if (new.tipo_id = 2 OR new.tipo = 0) then
+        if (new.tipo_id = 2 OR new.tipo_id = 0) then
             if (new.valor <> old.valor) then
                 select * into v_carteira from carteira where id = new.carteira_id;
                 v_old = v_carteira.despesa - old.valor;
@@ -82,9 +80,12 @@ begin
         return new;
     end if;
 end;
-$body$
-    language plpgsql;
+$$;
+
+alter function atualizar_valor_despesa_update_despesa() owner to postgres;
+
+
 
 create trigger atualizar_valor_despesa_update_despesa
-    after UPDATE on despesa
+    after INSERT OR UPDATE on despesa
     for each row execute function atualizar_valor_despesa_update_despesa()
