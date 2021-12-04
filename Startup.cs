01c8313546e833bf78d32c3d5baf5165ca969742
Bruno Batista <brunorafael.o@hotmail.com>
2021-12-04 14:31:43 -0300

    Adiciona configuracoes para uso no apache com proxy

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -89,6 +90,10 @@ namespace Tequila
                 .AllowAnyMethod()
                 .AllowAnyHeader());
 
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
             app.UseAuthentication();
             app.UseAuthorization();
             // app.UseOptions();
